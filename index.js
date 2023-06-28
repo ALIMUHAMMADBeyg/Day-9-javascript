e14491affe2842640db84d68cc9ec6106424e64e
ALIMUHAMMADBeyg <131273856+ALIMUHAMMADBeyg@users.noreply.github.com>
2023-06-28 00:57:19 -0700

    Add files via upload
    vanilla javascript

---
+++ index.js
@@ -0,0 +1,45 @@
+let message = "Hey! Javascriopt";
+console.log("Hello Javascript");
+
+// Values and variables in javascript//
+
+const myName = "Ajax";
+let myage = 99;
+console.log(myName);
+
+//If Else Statements
+//if rain = raincaot 
+//Else no RainCoat 
+
+/*var tommorow = 'rain';
+var tommorow = 'sunny';
+if (tommorow == 'rain') { 
+    console.log('It is rainny so take a umbrella');
+
+}else{
+    console.log('Its Not Rainny but if there is sunny then still take a umbrella either ')
+}*/
+
+//conditions
+
+var Tasks = 'research Data legitimity professionalism and smartwork along with common sense intellegence';
+
+if (Tasks == 'research Data legitimity professionalism and smartwork along with common sense intellegence'){
+    console.log('reasearch Data legitimity professionalism and smartwork along with common sense intellegence');
+
+}else {
+    ('Focuse to the core');  //this not actually needed but still i resist or should say i did 
+    console.log('Focus to the core');
+}
+
+var year = 2023;
+//condition
+
+if (year % 4 ==0){
+    console.log("the yaer" + year + "is a leap year");
+
+}else{
+    console.log ("The year" + year + "is not a leap year");
+    
+}
+;
